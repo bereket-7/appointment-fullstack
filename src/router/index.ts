ebf67d421aa635ad872807ddc3712e27db1f6012
bereket-7 <bekhonelign@gmail.com>
2025-06-09 02:06:57 +0300

    Refactor router configuration to include child routes for Dashboard, Users, and Settings

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import DashboardHome from '@/views/DashboardHome.vue'
+import UserPage from '@/views/UserPage.vue'
+import SettingPage from '@/views/SettingPage.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Dashboard',
     component: DashboardHome,
+     children: [
+      { path: '', name: 'Dashboard', component: DashboardHome },
+      { path: 'users', name: 'Users', component: UserPage },
+      { path: 'settings', name: 'Settings', component: SettingPage }
+    ]
   },
 ]
 
