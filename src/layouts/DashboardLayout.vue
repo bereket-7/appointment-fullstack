8c70c4a99557a79972204526c3bbd61ea0582765
bereket-7 <bekhonelign@gmail.com>
2025-06-09 01:18:55 +0300

    Add DashboardLayout component with template structure and layout elements

---
+++ src/layouts/DashboardLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex h-screen bg-gray-100">
+    <Sidebar />
+    <div class="flex flex-col flex-1 overflow-hidden">
+      <Topbar />
+      <main class="p-4 overflow-y-auto flex-1">
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Sidebar from '@/components/Sidebar.vue'
+import Topbar from '@/components/Topbar.vue'
+</script>
