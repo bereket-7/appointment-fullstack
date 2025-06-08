476dbb5986f2429fcd46664a15f6aec284086164
bereket-7 <bekhonelign@gmail.com>
2025-06-09 01:17:38 +0300

    Add Sidebar component with template structure and navigation links

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,10 @@
+<!-- components/Sidebar.vue -->
+<template>
+  <aside class="w-64 bg-white shadow-md p-4">
+    <nav class="space-y-2">
+      <RouterLink to="/" class="block p-2 rounded hover:bg-gray-100">Dashboard</RouterLink>
+      <RouterLink to="/users" class="block p-2 rounded hover:bg-gray-100">Users</RouterLink>
+      <RouterLink to="/settings" class="block p-2 rounded hover:bg-gray-100">Settings</RouterLink>
+    </nav>
+  </aside>
+</template>
