a1187381a6ddfec5fe840d3e319209b56671f3f3
bereket-7 <bekhonelign@gmail.com>
2025-06-09 01:52:54 +0300

    Refactor App.vue to use router-view and update Tailwind CSS configuration for content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: ["class"],
-    content: [],
+	content: [
+  './index.html',
+  './src/**/*.{vue,js,ts,jsx,tsx}',
+	  './node_modules/@shadcn/ui/dist/**/*.{js,ts,vue}',],
   theme: {
   	extend: {
   		borderRadius: {
