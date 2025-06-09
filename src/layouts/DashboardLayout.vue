1ab2e7e4f2f050592905f3b807aff85f9417de3b
bereket-7 <bekhonelign@gmail.com>
2025-06-10 01:43:52 +0300

    Refactor DashboardLayout component for improved structure and layout consistency

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -1,18 +1,23 @@
-<!-- src/layouts/DashboardLayout.vue -->
+<!-- src/components/layout/DashboardLayout.vue -->
 <script setup lang="ts">
+import AppSidebar from "@/components/AppSidebar.vue";
+import AppTopbar from "@/components/AppTopbar.vue";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import AppSidebar from "@/components/AppSidebar.vue"
+
 </script>
 
 <template>
   <SidebarProvider>
     <div class="flex min-h-screen">
       <AppSidebar />
-
-      <main class="flex-1 p-6">
-        <SidebarTrigger />
-        <RouterView />
-      </main>
+      <div class="flex-1 flex flex-col w-full">
+        <AppTopbar />
+        <!-- <SidebarTrigger /> -->
+        <main class="flex-1 p-2 overflow-y-auto">
+          <RouterView />
+        </main>
+      </div>
     </div>
   </SidebarProvider>
 </template>
+
