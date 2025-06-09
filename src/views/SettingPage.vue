351ad015aa392ff6fee1dcb95d15b093cb768851
bereket-7 <bekhonelign@gmail.com>
2025-06-10 00:39:38 +0300

    Refactor SettingPage component to improve header and maintain structure

--- src/views/SettingPage.vue
+++ src/views/SettingPage.vue
@@ -1,7 +1,11 @@
+<script setup lang="ts">
+import AccountSetting from '@/components/settings/AccountSetting.vue';
+
+</script>
+
 <template>
   <div>
-    <h2 class="text-2xl font-bold mb-4">Settings</h2>
-    <h2 class="text-2xl font-bold mb-4">Configure your application settings here!</h2>
-    <!-- settings UI here -->
+    <h2 class="text-2xl mb-4">Configure your application settings here!</h2>
+    <AccountSetting />
   </div>
 </template>
