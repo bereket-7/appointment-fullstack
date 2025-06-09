95d31e9e6160d8bd53de1f49ab651c389f6a7252
bereket-7 <bekhonelign@gmail.com>
2025-06-10 01:42:10 +0300

    Refactor UserPage component to include UserTable and UserForm for improved user management interface

--- src/views/UserPage.vue
+++ src/views/UserPage.vue
@@ -1,7 +1,13 @@
+<script setup lang="ts">
+import UserForm from '@/components/users/UserForm.vue';
+import UserTable from '@/components/users/UserTable.vue';
+
+</script>
+
 <template>
   <div>
     <h2 class="text-2xl font-bold mb-4">User Management</h2>
-    <!-- user management UI here -->
+    <UserTable />
+     <UserForm />
   </div>
 </template>
-
