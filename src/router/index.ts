0f83fc3575c48c63cddbac4052d75d2fd30ae712
bereket-7 <bekhonelign@gmail.com>
2025-06-09 10:24:59 +0300

    Refactor router configuration to use DashboardLayout and organize child routes for improved structure

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,16 +2,25 @@ import { createRouter, createWebHistory } from 'vue-router'
 import UserPage from '@/views/UserPage.vue'
 import SettingPage from '@/views/SettingPage.vue'
 import DashboardHome from '@/views/DashboardHome.vue'
+import DashboardLayout from '@/layouts/DashboardLayout.vue'
+import EventsPage from '@/views/EventsPage.vue'
+import CalendarPage from '@/views/CalendarPage.vue'
+import AppointmentPage from '@/views/AppointmentPage.vue'
+
 
 const routes = [
   {
     path: '/',
-    component: DashboardHome,
-     children: [
+    component: DashboardLayout,
+    children: [
+      { path: '', name: 'Home', component: DashboardHome },
+      { path: 'appointments', name: 'Appointments', component: AppointmentPage },
       { path: 'users', name: 'Users', component: UserPage },
-      { path: 'settings', name: 'Settings', component: SettingPage }
+      { path: 'calendar', name: 'Calendar', component: CalendarPage },
+      { path: 'events', name: 'Events', component: EventsPage },
+      { path: 'settings', name: 'Settings', component: SettingPage },
     ]
-  },
+  }
 ]
 
 const router = createRouter({
